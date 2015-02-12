1763123569ee08077a6cf347afc4c1fba2867eba
Moshe Zada <moshe@forter.com>
2015-02-12 15:51:47 +0200

    delete comment

--- src/main/java/com/forter/monitoring/RiemannListener.java
+++ src/main/java/com/forter/monitoring/RiemannListener.java
@@ -67,7 +67,6 @@ public class RiemannListener extends TestListenerAdapter{
 
         if (Discovery.instance().isAWS()) {
             connect();
-            //String CommitHash = getGitHash();
             if (state.equals("failed")) {
                 tr.getThrowable().printStackTrace(new PrintWriter(errors));
                 description = errors.toString();
